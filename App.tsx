479ffd40084b463b1e9302ff8ebe713691970365
yoongyum <imshreg@naver.com>
2022-05-14 18:38:37 +0900

    feat: Create App.tsx

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is HelloWord!</Text>
+      <Text>This is HelloWord!!</Text>
       <StatusBar style="auto" />
     </View>
   );

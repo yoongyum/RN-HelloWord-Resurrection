2a95379353918182d6730cdb74887fdcb7104a15
yoongyum <imshreg@naver.com>
2022-05-21 21:09:42 +0900

    feat: link HomeNavigator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './homeScreen/homeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeNavigator from './src/navigators/HomeNavigation';
 
 
 export default function App() {
   return (
-    <HomeScreen/> 
+    <NavigationContainer>
+        <HomeNavigator/>
+    </NavigationContainer>
   );
 }
 

e48f3fb7ce9346eef240fa168e123e47b9a2ef2b
yoongyum <imshreg@naver.com>
2022-05-21 14:47:00 +0900

    feat: Create HomeScreen.tsx
    홈스크린용 컴포넌트를 새성

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './homeScreen/homeScreen';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is HelloWord!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomeScreen/> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
